438029eecaf909c97218b8945e97a22e1aaf190e
Kareigi <wkareigi@gmail.com>
2024-02-20 14:32:31 +0300

    fourth commit

---
+++ week_1/calculator.py
@@ -0,0 +1,14 @@
+#simple calculator program
+#Date : 20/02/2024
+#Name: Wayne Kareigi
+
+
+f_number =input("Enter the first number   :   ")
+s_number = input("Enter the second number   :  ")
+
+
+print(int(f_number) + int(s_number))
+print(int(f_number) - int(s_number))
+print(int(f_number) * int(s_number))
+print(int(f_number) / int(s_number))
+print(int(f_number) % int(s_number))
---
+++ week_1/quadratics.py
@@ -0,0 +1,16 @@
+#Program solving quadratic equations
+#Date : 20/02/2024
+#Name: Wayne Kareigi
+import math
+
+a = float(input("Enter the coefficient of the first term : "))
+b =float(input("Enter the coefficient of second term : "))
+c = float(input("Enter the constant : "))
+
+d = (b**2) - 4 * a * c
+
+x_1 = (-b + math.sqrt(d) ) /2* a
+x_2 =(-b - math.sqrt(d) ) /2* a
+
+print("The solution of the quadratic equation", x_1 )
+print("The solution of the quadratic equation", x_2)
